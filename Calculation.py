6fe643dc8bfd61786575891dbea4fe71f1b0995a
Iyarr <8.24ridiculous@gmail.com>
2022-04-19 14:08:17 +0900

    first commit

---
+++ Calculation.py
@@ -0,0 +1,25 @@
+import math
+def dataget():
+    rowct=0
+    columnct = 0
+    queue = []
+    while(1):
+        row = input("対応する行列を入力してください")
+        columnct = max(columnct,row.count(" ")+1)
+        
+        if( row == ""):
+            break
+        queue.append(row)
+        rowct = rowct + 1
+    cell = ["NULL"]*rowct*columnct
+    for ct1 in range(rowct):
+        cell[ct1] = ["NULL"] * columnct
+        ct2 = 0
+        for values in queue[ct1].split(" "):
+            cell[ct1][ct2] = values
+            ct2 = ct2 + 1
+
+#    for ct1 in range(rowct):
+#        print(cell[ct1])
+    return cell
+data = dataget()
