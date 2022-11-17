c55e470afd75ac42e165f2385b028fa2667d72ac
Iyarr <8.24ridiculous@gmail.com>
2022-11-17 09:44:07 +0900

    improve make_rpm

--- rpm_test.py
+++ rpm_test.py
@@ -2,7 +2,7 @@ class Method:
 #   逆ポーランド記法への変換
     def convert_to_rpn(self,expr):
         length = len(expr)
-        if length < 3:
+        if length < 2 or expr.isdigit():
             return expr
 
         if self.find_brackets(expr) == length-1:
@@ -25,6 +25,8 @@ class Method:
 
         # 演算子の検出　＋、ー
     def find_add_sub(self,expr):
+        if len(expr) < 2 or expr.isdigit():
+            return None
         result = []
         code = []
         code_ct = 0
@@ -52,7 +54,7 @@ class Method:
 
         # 演算子の検出　＊ need to improve
     def find_mul(self,expr):
-        if len(expr) < 2:
+        if len(expr) < 2 or expr.isdigit():
             return None
         result = []
         code_ct = 0
@@ -60,16 +62,16 @@ class Method:
         st = 0
         for ct, c in enumerate(expr):
             deep = self.deep_process(deep,c)
-            if deep == 0:
+            if deep == 0 and c != '-':
                 if c == '*':
-                    st += 1
+                    st = ct + 1
 
                 elif ct >= len(expr)-1:
-                    result.append(self.convert_to_rpn(self,expr[st:ct+1]))
+                    result.append(self.convert_to_rpn(self,expr[st:]))
                     code_ct += 1
                     st = ct + 1
 
-                elif (( c.isdigit() or c == '-' ) and expr[ct+1].isdigit()) == False:
+                elif ( c.isdecimal() and expr[ct+1].isdecimal()) == False:
                     result.append(self.convert_to_rpn(self,expr[st:ct+1]))
                     code_ct += 1
                     st = ct + 1
@@ -77,8 +79,15 @@ class Method:
                 if( code_ct > 1 ):
                     result.append('*')
                     code_ct = 1
-                    st = ct + 1
         return result
+
+        # 再帰関数の深さ
+    def deep_process(deep,c):
+        if c == '(':
+            deep += 1
+        elif c == ')':
+            deep -= 1
+        return deep
 
         # 2桁以上の数値のデータをつなげる
     def next_data(c,next_c):
@@ -88,14 +97,6 @@ class Method:
 
         return result
 
-
-        # 再帰関数の深さ
-    def deep_process(deep,c):
-        if c == '(':
-            deep += 1
-        elif c == ')':
-            deep -= 1
-        return deep
 
 #   list型をstr型に
     def convert_to_str(self,list):
